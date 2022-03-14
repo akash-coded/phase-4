e874b24fa98982d4412b1462ac603e656cff321b
Akash Das <mfs.akash@gmail.com>
2022-03-15 00:10:10 +0530

    added examples with exports and window function

--- js/functions/callback-functions.js
+++ js/functions/callback-functions.js
@@ -1,10 +1,11 @@
-/**
- * The function should ask the question and, depending on the user’s answer,
- * call yes() or no()
- * @param {string} question
- * @param {Function} yes
- * @param {Function} no
- */
+function showOk() {
+  alert("You agreed.");
+}
+
+function showCancel() {
+  alert("You disagreed.");
+}
+
 function ask(question, yes, no) {
   // Function declaration
   if (confirm(question)) {
@@ -14,24 +15,4 @@ function ask(question, yes, no) {
   }
 }
 
-function showOk() {
-  alert("You agreed.");
-}
-
-function showCancel() {
-  alert("You disagreed.");
-}
-
-// usage: functions showOk, showCancel are passed as arguments to ask
-ask("Do you agree?", showOk, showCancel);
-
-// using function expressions (anonymous functions) as callback functions
-ask(
-  "Do you like ice-cream?",
-  function () {
-    alert("Good choice!");
-  },
-  function () {
-    alert("Try one");
-  }
-);
+export { showOk, showCancel, ask };
